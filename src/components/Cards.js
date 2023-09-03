7981886a78a0917cdef87fb6f86fa12f030ee526
saeed joulaei <saeedjoulaei86@gmail.com>
2023-09-03 10:14:15 +0330

    images replaced in cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import styles from "./Cards.module.css";
-import mobile from "../images/pngfind.com-samsung-mobile-png-948177.png";
+import iphoneX from "../images/iphone10.jpg";
+import iphone11 from "../images/iphone11.jpg";
+import iphone12 from "../images/iphone12.jpg";
+import s21 from "../images/s21.jpg";
 import Card from "./Card";
 class Cards extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <Card image={mobile} name="sumsung" cost="500 $" />
-        <Card image={mobile} name="iphone x" cost="600 $" />
-        <Card image={mobile} name="lg 65x" cost="400 $" />
-        <Card image={mobile} name="note 11" cost="300 $" />
+        <Card image={iphoneX} name="sumsung" cost="500 $" />
+        <Card image={iphone11} name="iphone x" cost="600 $" />
+        <Card image={iphone12} name="lg 65x" cost="400 $" />
+        <Card image={s21} name="note 11" cost="300 $" />
       </div>
     );
   }
