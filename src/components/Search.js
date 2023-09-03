90a76619868025d3ae96232e08da834854cf4db8
saeed joulaei <saeedjoulaei86@gmail.com>
2023-09-03 13:34:13 +0330

    search input changed to display value below

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 import styles from "./Search.module.css";
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: "",
+    };
+  }
+  inputHandler = (event) => {
+    this.setState({
+      text: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div className={styles.container}>
         <p>search what you want</p>
-        <input placeholder="search..." />
+        <div className={styles.searchSection}>
+          <input
+            placeholder="search..."
+            value={this.state.text}
+            onChange={this.inputHandler}
+          />
+          <br />
+          <span>{this.state.text}</span>
+        </div>
       </div>
     );
   }
