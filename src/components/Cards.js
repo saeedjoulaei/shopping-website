605017eb4b6d7f6e3ed523c88f80085f50b32354
saeed joulaei <saeedjoulaei86@gmail.com>
2023-09-04 11:26:39 +0330

    map method and uniqe keys added to cards to be more efficient

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,13 +6,28 @@ import iphone12 from "../images/iphone12.jpg";
 import s21 from "../images/s21.jpg";
 import Card from "./Card";
 class Cards extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: [
+        { id: 1, image: iphoneX, name: "sumsung", cost: "500 $" },
+        { id: 2, image: iphone11, name: "iphone x", cost: "600 $" },
+        { id: 3, image: iphone12, name: "lg 65x", cost: "400 $" },
+        { id: 4, image: s21, name: "note 11", cost: "300 $" },
+      ],
+    };
+  }
   render() {
     return (
       <div className={styles.container}>
-        <Card image={iphoneX} name="sumsung" cost="500 $" />
-        <Card image={iphone11} name="iphone x" cost="600 $" />
-        <Card image={iphone12} name="lg 65x" cost="400 $" />
-        <Card image={s21} name="note 11" cost="300 $" />
+        {this.state.userData.map((car) => (
+          <Card
+            key={car.id}
+            image={car.image}
+            name={car.name}
+            cost={car.cost}
+          />
+        ))}
       </div>
     );
   }
